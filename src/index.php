96cf23becd7703e7ba1cbd060b1bbc856f3601e6
Steve Saner <steve@saner.net>
2020-01-15 23:39:21 -0600

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <title>LRN Lookup</title>
+ </head>
+ <body>
+  <form name="lrnlookup" action="lookup.php">
+   <label for="dids">Enter DIDs</label>
+   <textarea id="dids" name="dids" cols="20" rows="20"></textarea>
+   <button type="submit" value="Lookup">Lookup</button>
+   <button type="reset" value="Clear">Clear</button>
+  </form>
+ </body>
+</html>
