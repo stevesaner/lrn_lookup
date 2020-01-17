1fa491d7db1c010cac0738991bbe3c2c705e4a17
Steve Saner <steve@saner.net>
2020-01-16 21:13:55 -0600

    Add some style to page

--- src/index.php
+++ src/index.php
@@ -2,19 +2,24 @@
 <html lang="en">
  <head>
   <title>LRN Lookup</title>
+  <link rel='stylesheet' href='site.css'>
  </head>
  <body>
-  <form name="lrnlookup" action="lookup.php">
-   <div>
-    <label for="dids">Enter DIDs</label>
-   </div>
-   <div>
-    <textarea id="dids" name="dids" cols="20" rows="20"></textarea>
-   </div>
-   <div>
-    <button type="submit" value="Lookup">Lookup</button>
-    <button type="reset" value="Clear">Clear</button>
-   </div>
-  </form>
+  <div id="main">
+   <form name="lrnlookup" action="lookup.php">
+    <div class="group">  
+     <div>
+      <label for="dids">Enter DIDs</label>
+     </div>
+     <div>
+      <textarea id="dids" name="dids" cols="20" rows="20"></textarea>
+     </div>
+    </div>
+    <div class="group">
+     <button type="submit" value="Lookup">Lookup</button>
+     <button type="reset" value="Clear">Clear</button>
+    </div>
+   </form>
+  </div>
  </body>
 </html>
